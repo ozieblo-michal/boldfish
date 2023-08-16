521243d26c641e6ce62a2e11d739533f78c50ef9
ozieblo <ozieblo.michal@gmail.com>
2023-08-16 23:50:46 +0200

    License update, bionic_reading_revise dccstring
    slight powdering

--- app/bionic_reading_revise.py
+++ app/bionic_reading_revise.py
@@ -6,6 +6,16 @@ import logging
 def bionic_reading_revise(
     definitions: dict, BIONIC_READING_X_RAPID_API_KEY: str
 ) -> dict:
+    """Uses the Bionic Reading API to format each definition text 
+
+    Args:
+        definitions (dict): concepts and definitions given as a plain string
+        BIONIC_READING_X_RAPID_API_KEY (str): 
+        from https://rapidapi.com/bionic-reading-bionic-reading-default/api/bionic-reading1
+
+    Returns:
+        dict: origin dictionary with values (definitions) overwritten in HyperText Markup Language
+    """
     logger = logging.getLogger("configure_logger")
 
     conn = http.client.HTTPSConnection(constants.BIONIC_READING_API_URL)
